e6f5e047d3141be7fac6cd700f2c0723f968387b
Rouvb <rouvbkim@gmail.com>
2024-04-27 20:15:49 +0900

    first commit

---
+++ updater.py
@@ -0,0 +1,26 @@
+import paramiko
+import os
+
+
+# FTP Connection Information
+sftp_host = ""
+sftp_port = ""
+sftp_user = ""
+sftp_password = ""
+
+# Connect to SSH
+ssh_client = paramiko.SSHClient()
+ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh_client.connect(sftp_host, sftp_port, sftp_user, sftp_password)
+sftp_client = ssh_client.open_sftp()
+print("SSH has been connected.")
+
+# Get plugins from your local folder.
+localfiles = os.listdir("./plugins")
+
+# Upload plugins to SFTP servers.
+for i in range(len(localfiles)):
+    localpath = f"./plugins/{localfiles[i]}"
+    remotepath = f"./test-plugins/{localfiles[i]}"
+    sftp_client.put(localpath, remotepath)
+    print(f"{localfiles[i]} has been uploaded.")
