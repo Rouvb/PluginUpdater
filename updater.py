d590a14125dde5ff9bd87ce260a262c2d4010788
Rouvb <rouvbkim@gmail.com>
2024-04-28 16:48:58 +0900

    Add old version plugin remover

--- updater.py
+++ updater.py
@@ -1,35 +1,59 @@
+import re
 import paramiko
 import os
 import json
 
+def extract_version(filename):
+    match = re.search(r"[-\s]*v?\s*(\d+(?:\.\d+)*)\.jar$", filename, re.IGNORECASE)
+    return match.group(1) if match else None
 
-with open('sftp.json', 'r') as f:
+def extract_base_name_and_version(filename):
+    version = extract_version(filename)
+    if version:
+        base_name = re.sub(r"[-\s]*v?\s*\d+(?:\.\d+)*\.jar$", "", filename, flags=re.IGNORECASE)
+        return base_name.lower(), version
+    else:
+        return filename.lower(), None
+
+with open("sftp.json", "r") as f:
     json_data = json.load(f)
 
-# Get plugins from your local folder.
 localfiles = os.listdir("./plugins")
 
 try:
-    for i in range(len(json_data)):
-        # Get SFTP connection profile from sftp.json
-        name = json_data[i]['name']
-        host = json_data[i]['host']
-        port = json_data[i]['port']
-        username = json_data[i]['username']
-        password = json_data[i]['password']
-        
-        # Connect to SSH
+    for config in json_data:
         ssh_client = paramiko.SSHClient()
         ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh_client.connect(host, port, username, password)
+        ssh_client.connect(config["host"], config["port"], config["username"], config["password"])
         sftp_client = ssh_client.open_sftp()
 
-        # Upload plugins to SFTP servers.
-        for j in range(len(localfiles)):
-            localpath = f"./plugins/{localfiles[j]}"
-            remotepath = f"./plugins/{localfiles[j]}"
-            sftp_client.put(localpath, remotepath)
-            print(f"{name}: {localfiles[j]} has been uploaded.")
+        remote_files = sftp_client.listdir("./plugins")
+
+        # Remove old version plugins
+        for localfile in localfiles:
+            base_name, local_version = extract_base_name_and_version(localfile)
             
+            if local_version:
+                for remote_file in remote_files:
+                    remote_base_name, remote_version = extract_base_name_and_version(remote_file)
+                    # Remove old version plugin if local version and remote version is not matched
+                    if base_name == remote_base_name and local_version != remote_version:
+                        sftp_client.remove(f"./plugins/{remote_file}")
+                        print(f"{config['name']}: {remote_file} has been removed.")
+                        remote_files.remove(remote_file) 
+
+        # Upload file
+        for localfile in localfiles:
+            localpath = f"./plugins/{localfile}"
+            remotepath = f"./plugins/{localfile}"
+            sftp_client.put(localpath, remotepath)
+            print(f"{config['name']}: {localfile} has been uploaded.")
+
 except Exception as e:
     print(e)
+
+finally:
+    if sftp_client:
+        sftp_client.close()
+    if ssh_client:
+        ssh_client.close()
